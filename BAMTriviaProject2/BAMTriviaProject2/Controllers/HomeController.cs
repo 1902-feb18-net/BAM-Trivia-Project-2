6928a4d95a554fa8c4a331fbe462e642df06c174
Your Name <mpkagel@gmail.com>
2019-03-15 15:48:12 -0500

    Switched the VS project out for a stock Web App

--- BAMTriviaProject2/BAMTriviaProject2/Controllers/HomeController.cs
+++ BAMTriviaProject2/BAMTriviaProject2/Controllers/HomeController.cs
@@ -4,9 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Project2.Models;
+using BAMTriviaProject2.Models;
 
-namespace Project2.Controllers
+namespace BAMTriviaProject2.Controllers
 {
     public class HomeController : Controller
     {

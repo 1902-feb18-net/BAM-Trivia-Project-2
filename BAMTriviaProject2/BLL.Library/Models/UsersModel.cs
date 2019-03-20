50ba00df9a688f67040a26bf116e3cb9b9044e31
Impstar <magesbe@gmail.com>
2019-03-20 09:57:57 -0500

    Finished mapping function

--- BAMTriviaProject2/BLL.Library/Models/UsersModel.cs
+++ BAMTriviaProject2/BLL.Library/Models/UsersModel.cs
@@ -6,5 +6,13 @@ namespace BLL.Library.Models
 {
     public class UsersModel
     {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Pw { get; set; }
+        public string Username { get; set; }
+        public long? CreditCardNumber { get; set; }
+        public int PointTotal { get; set; }
+        public bool? AccountType { get; set; }
     }
 }

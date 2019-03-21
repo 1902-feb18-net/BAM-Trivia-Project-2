7740820a2ddf1238e1f7da006d4fa1e74984b094
Your Name <mpkagel@gmail.com>
2019-03-20 23:26:13 -0500

    Gave this API some Swagger

--- BAMTriviaProject2/BAMTriviaProject2.WebAPI/Startup.cs
+++ BAMTriviaProject2/BAMTriviaProject2.WebAPI/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace BAMTriviaProject2.WebAPI
 {
@@ -34,8 +35,17 @@ namespace BAMTriviaProject2.WebAPI
             services.AddDbContext<BAMTriviaProject2Context>(builder =>
                 builder.UseSqlServer(Configuration.GetConnectionString("BAMTriviaProject2")));
 
+            services.AddMvc(options =>
+            {
+                options.ReturnHttpNotAcceptable = true;
+            })
+            //.AddXmlSerializerFormatters()
+            .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +62,17 @@ namespace BAMTriviaProject2.WebAPI
             }
 
             app.UseHttpsRedirection();
+
+
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             app.UseMvc();
         }
     }

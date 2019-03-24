db18102c6eb94d0fabfcaec50cc3180e01e03d53
Your Name <mpkagel@gmail.com>
2019-03-23 21:03:00 -0500

    Added logins to the API and AuthDb

---
+++ BAMTriviaProject2/BAMTriviaProject2.WebAPI/AuthModels/AuthAccountDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BAMTriviaProject2.WebAPI.AuthModels
+{
+    public class AuthAccountDetails
+    {
+        public string Username { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
